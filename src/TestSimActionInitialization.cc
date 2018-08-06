6d07ce846a5a892d7ced1fb83202964724190c24
Soumya Ghosh <ghoshss93@gmail.com>
2018-08-06 15:48:08 -0400

    Initial commit

---
+++ src/TestSimActionInitialization.cc
@@ -0,0 +1,71 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: TestSimActionInitialization.cc 68058 2013-03-13 14:47:43Z gcosmo $
+//
+/// \file TestSimActionInitialization.cc
+/// \brief Implementation of the TestSimActionInitialization class
+
+#include "TestSimActionInitialization.hh"
+#include "TestSimPrimaryGeneratorAction.hh"
+#include "TestSimRunAction.hh"
+#include "TestSimEventAction.hh"
+#include "TestSimSteppingAction.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+TestSimActionInitialization::TestSimActionInitialization()
+ : G4VUserActionInitialization()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+TestSimActionInitialization::~TestSimActionInitialization()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void TestSimActionInitialization::BuildForMaster() const
+{
+  TestSimRunAction* runAction = new TestSimRunAction;
+  SetUserAction(runAction);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void TestSimActionInitialization::Build() const
+{
+  SetUserAction(new TestSimPrimaryGeneratorAction);
+
+  TestSimRunAction* runAction = new TestSimRunAction;
+  SetUserAction(runAction);
+  
+  TestSimEventAction* eventAction = new TestSimEventAction(runAction);
+  SetUserAction(eventAction);
+  
+  SetUserAction(new TestSimSteppingAction(eventAction));
+}  
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
