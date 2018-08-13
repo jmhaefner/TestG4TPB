9d66aac243e6d8ef2aaadcf8d489e2ed37730595
Soumya Ghosh <ghoshss93@gmail.com>
2018-08-13 09:54:14 -0400

    Change particle gun output to opticalphoton
    Particle gun output was previously "gamma" in PrimaryGeneratorAction.
    Also changed the output of the particle gun in run2.mac to opticalphoton
    (at least for the first run). Note that I'm not certain that
    "opticalphoton" is the correct name for the object; this version does
    compile and run though.

--- src/TestSimPrimaryGeneratorAction.cc
+++ src/TestSimPrimaryGeneratorAction.cc
@@ -54,7 +54,7 @@ TestSimPrimaryGeneratorAction::TestSimPrimaryGeneratorAction()
   G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
   G4String particleName;
   G4ParticleDefinition* particle
-    = particleTable->FindParticle(particleName="gamma");
+    = particleTable->FindParticle(particleName="opticalphoton");
   fParticleGun->SetParticleDefinition(particle);
   fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,1.));
   fParticleGun->SetParticleEnergy(6.*MeV);
